43f5794409fc55e4ac1475289579b16f5497fe71
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-05 12:49:24 -0500

    Parent to Child relations / passing info

--- components/src/index.js
+++ components/src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import faker from 'faker';
 
 const App = () => {
-    return (
-      <div className="ui container comments">
-          
-          <CommentDetail />
-        
-      </div>  
-
-    );
+	return (
+		<div className="ui container comments">
+			<CommentDetail
+				avatar={faker.image.image()}
+				author="Sam"
+				timeAgo="Today at 4:45PM"
+				comment="Hello I'm Sam!"
+			/>
+			<CommentDetail
+				avatar={faker.image.image()}
+				author="Alfred"
+				timeAgo="Today at 5:50PM"
+				comment="Hey Sam, welcome."
+			/>
+			<CommentDetail
+				avatar={faker.image.image()}
+				author="John"
+				timeAgo="Today at 5:51PM"
+				comment="Look forward to working with you. "
+			/>
+			<CommentDetail
+				avatar={faker.image.image()}
+				author="Stephon"
+				timeAgo="Today at 7:00PM"
+				comment="Nice to meet you."
+			/>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
