98a3a3a237a5bdda277ae0e1e8140acbbe4687ee
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-05 13:14:55 -0500

    Wrap an Approval Card around a CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,14 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<ApprovalCard />
-			<CommentDetail
-				avatar={faker.image.image()}
-				author="Sam"
-				timeAgo="Today at 4:45PM"
-				comment="Hello I'm Sam!"
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					avatar={faker.image.image()}
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					comment="Hello I'm Sam!"
+				/>
+			</ApprovalCard>
 			<CommentDetail
 				avatar={faker.image.image()}
 				author="Alfred"
