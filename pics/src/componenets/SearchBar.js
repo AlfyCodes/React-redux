138ab97e9f9f5cd281bcc7ae05893e8a403aca19
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-08 14:38:33 -0500

    Prevent default submit, log the result (error)

--- pics/src/componenets/SearchBar.js
+++ pics/src/componenets/SearchBar.js
@@ -4,10 +4,17 @@ class SearchBar extends React.Component {
 
     state = {term: ''}
 
+    onFormSubmit(event){
+
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Image Search</label>
                         {/*Do not put () - we only want to callback onInputChange when needed () will call every time! */}
