d96bdabbb73b5a24ce0098f55ee6c92ab0967279
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-09 12:36:59 -0500

    DataFetching in react Refac onSearchSubmit to bind

--- pics/src/componenets/App.js
+++ pics/src/componenets/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import axios from 'axios';
 
 import SearchBar from './SearchBar';
-
 class App extends React.Component {
-
-	state = {images: []}
-	async onSearchSubmit(term) {
-		 const response = await axios.get('https://api.unsplash.com/search/photos', {
+	state = { images: [] };
+	onSearchSubmit = async (term) => {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization: 'Client-ID FF1EXdUQ1WI1MB6KACEwm-d8M9JKbKEYjpYjx1Rsgfw'
 			}
 		});
-		console.log(this)
 		this.setState({ images: response.data.results});
-	}
+	};
 
 	render() {
+		
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
