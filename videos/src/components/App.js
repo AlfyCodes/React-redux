f28bf0c1110e3adb2035a6bfe6d6a1d9cb67b1e6
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-10 14:02:15 -0500

    +styleref+ui container to app.js +build searchBar

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
+
+	
 	render() {
 		return (
-			<div>
+			<div className=" ui container">
 				<SearchBar />
 			</div>
 		);
