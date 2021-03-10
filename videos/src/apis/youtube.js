5a02a2f5e0cabfc99ea0c1c70ff04acd3c56c427
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-10 15:37:41 -0500

    Install axios, build YT api to handle request.

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,14 @@
-const KEY = 'AIzaSyCvQkso2Ee4FB7bHYW1GBdXOQPKAl0BPK4'
+import axios from 'axios';
+
+const KEY = 'AIzaSyCvQkso2Ee4FB7bHYW1GBdXOQPKAl0BPK4';
+
+export default axios.create({
+
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+
+});
