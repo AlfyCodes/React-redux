55484f3a36296164be604b68ac5d8ed812400471
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-12 14:37:21 -0500

    logic to handle click response => display context

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Accordion = ({items}) => {
-    const renderedItems = items.map((item, index) => {
+const Accordion = ({ items }) => {
+	
+    const [activeIndex, setActiveIndex] = useState(null);
+    
+    const onTitleClick = (index) => {
+		setActiveIndex(index);
+	};
 
-        const onTitleClick = (index) => {
+	const renderedItems = items.map((item, index) => {
+		return (
+			<React.Fragment key={item.title}>
+				<div className="title active" onClick={() => onTitleClick(index)}>
+					<i className="dropdown icon" />
+					{item.title}
+				</div>
+				<div className="content active">
+					<p>{item.content}</p>
+				</div>
+			</React.Fragment>
+		);
+	});
 
-            console.log('Title clicked', index);
-        };
-        
-
-        return(
-            <React.Fragment key={item.title}>
-                <div 
-                className="title active"
-                onClick={() => onTitleClick(index)}>
-                    <i className="dropdown icon"></i>
-                    {item.title}
-                </div>
-                <div className="content active">
-                    <p>{item.content}</p>
-                </div>
-            </React.Fragment>
-        )
-
-    });
-
-    return <div className= "ui styled accordion"> {renderedItems}</div>
+	return <div className="ui styled accordion"> {renderedItems}
+    <h1>{activeIndex}</h1>
+    </div>;
 };
 
 export default Accordion;
