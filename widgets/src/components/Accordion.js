94ff682bd82c36cd580ab615d18a973d4a7ac5f0
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-12 14:28:54 -0500

    Add onClick functionality

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Accordion = ({items}) => {
-    const renderedItems = items.map(item => {
+    const renderedItems = items.map((item, index) => {
+
+        const onTitleClick = (index) => {
+
+            console.log('Title clicked', index);
+        };
+        
 
         return(
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div 
+                className="title active"
+                onClick={() => onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
