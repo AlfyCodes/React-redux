1a38456a162125aee58fd4cca518e9340292642e
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-16 13:42:10 -0400

    add logic to handle intial search

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -21,16 +21,21 @@ const Search = () => {
 				setResults(data.query.search);
 			};
 
-			const timeoutId = setTimeout(() => {
-				if (term) {
-					search();
-				}
-			}, 1000);
+			if (term && !results.length){
 
-			return () => {
-				clearTimeout(timeoutId);
-			};
+				search();
 
+			}else {
+				const timeoutId = setTimeout(() => {
+					if (term) {
+						search();
+					}
+				}, 1000);
+	
+				return () => {
+					clearTimeout(timeoutId);
+				};
+			}
 		}, [ term ]
 	);
 
