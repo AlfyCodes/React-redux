1ed794256a51aeab739553c16091a741c9cdae4d
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-16 15:45:22 -0400

    add onClick func for dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Accordion from './components/Accordion';
 import Search from './components/Search';
@@ -20,26 +20,25 @@ const items = [
 ];
 
 const options = [
-{
-	label: 'The Color Red',
-	value: 'red'
-},
-{
-	
-	label: 'The Color Green',
-	value: 'green'
-},
-{
-	
-	label: 'A Shade of Blue',
-	value: 'blue'
-}
-
+	{
+		label: 'The Color Red',
+		value: 'red'
+	},
+	{
+		label: 'The Color Green',
+		value: 'green'
+	},
+	{
+		label: 'A Shade of Blue',
+		value: 'blue'
+	}
 ];
 export default () => {
+	const [ selected, setSelected ] = useState(options[0]);
+
 	return (
 		<div>
-			<Dropdown options={options} />
+			<Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
 		</div>
 	);
 };
