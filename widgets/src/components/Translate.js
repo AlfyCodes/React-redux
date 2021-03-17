d090713bfd11e2bd2f50e0047d2209801a912ce5
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-17 16:02:51 -0400

    fix dropdown to accept label prop, add input text

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,32 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
 const options = [
-{
-    label: 'Afrikaans',
-    balue: 'af'
-},
-{
-    label: 'Arabic',
-    value: 'ar',
-},
-{
-    label: 'Hindi',
-    value: 'hi'
-}
+	{
+		label: 'Afrikaans',
+		balue: 'af'
+	},
+	{
+		label: 'Arabic',
+		value: 'ar'
+	},
+	{
+		label: 'Hindi',
+		value: 'hi'
+	}
 ];
 
-
-
 const Translate = () => {
+	const [ language, setLanguage ] = useState(options[0]);
+	const [ text, setText ] = useState('');
 
-    const [language, setLanguage] = useState(options[0]);
-
-
-    return (
-        <div><Dropdown selected = {language} onSelectedChange = {setLanguage} options = {options} /></div>
-    );
-
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Text</label>
+					<input value={text} onChange={(e) => setText(e.target.value)} />
+				</div>
+			</div>
+			<Dropdown label="Select a Langauge" selected={language} onSelectedChange={setLanguage} options={options} />
+		</div>
+	);
 };
 
-export default Translate; 
+export default Translate;
