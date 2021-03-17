336b1856ad0374f0f000f698ece5645f8aeeac87
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-17 11:28:16 -0400

    add logic to close dropdown through eventListener

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+	const [ open, setOpen ] = useState(false);
 
-	const [open, setOpen] = useState(false);
+	useEffect(() => {
+		document.body.addEventListener('click', () => {
+			setOpen(false);
+		}, {capture: true});
+	}, []);
 
 	const renderedOptions = options.map((option) => {
-
-		if (option.value === selected.value){
+		if (option.value === selected.value) {
 			return null;
 		}
 		return (
-			<div key={option.value} className="item" onClick= {() => onSelectedChange(option)}>
+			<div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
 				{option.label}
 			</div>
 		);
@@ -20,7 +24,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 		<div className="ui form">
 			<div className="field">
 				<label className="label">Select a Color</label>
-				<div onClick={() => setOpen(!open)}className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+				<div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
 					<i className="dropdown icon" />
 					<div className="text">{selected.label}</div>
 					<div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
