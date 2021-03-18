081d5551de50ad0c39b6014fda0be2753135ca93
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-18 11:57:52 -0400

    Current issue with assigning Keys with dropdown

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 const Convert = ({language, text}) => {
     useEffect(() => {
 
+        axios.post('https://translation.googleapis.com/language/translate/v2', {});
         console.log('New Language or Text!')
 
     }, [language, text]);
-    return <div> Convert </div>
+    return <div />
 }
 
 export default Convert;
