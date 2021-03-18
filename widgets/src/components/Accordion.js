f02d5f465ee07af3b56c1c4f42c16d877022bf25
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-18 13:23:14 -0400

    fixed error: typo - fix code styling

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-	
-    const [activeIndex, setActiveIndex] = useState(null);
-    
-    const onTitleClick = (index) => {
-		setActiveIndex(index);
-	};
+  const [activeIndex, setActiveIndex] = useState(null);
 
-	const renderedItems = items.map((item, index) => {
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
 
-        const active = index === activeIndex ? 'active' : '';
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
 
-		return (
-			<React.Fragment key={item.title}>
-				<div className={`title ${active}`} onClick={() => onTitleClick(index)}>
-					<i className="dropdown icon" />
-					{item.title}
-				</div>
-				<div className={`content ${active}`}>
-					<p>{item.content}</p>
-				</div>
-			</React.Fragment>
-		);
-	});
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon" />
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-	return <div className="ui styled accordion"> {renderedItems} </div>;
+  return <div className="ui styled accordion"> {renderedItems} </div>;
 };
 
 export default Accordion;
--- widgets/src/index.js
+++ widgets/src/index.js
@@ -1,6 +1,6 @@
-import react from 'react';
-import ReactDOM from 'react-dom';
+import react from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
