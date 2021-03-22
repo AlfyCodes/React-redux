121d1fae30b28da3d8d0e143ed9bca1a1678326c
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-22 18:58:10 -0400

    Update navi url to match link pressed

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -5,6 +5,9 @@ const Link = ({ className, href, children }) => {
     event.preventDefault();
 
     window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
