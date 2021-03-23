d96bc7df1408ddb14450e08a14a8d3c89949d883
AlfyCodes <alfred.ortiz1@marist.edu>
2021-03-23 13:44:13 -0400

    reduce funtcion use

--- videos with hook/src/components/App.js
+++ videos with hook/src/components/App.js
@@ -26,10 +26,6 @@ const App = () => {
     setSelectedVideo(response.data.items[0]);
   };
 
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
   return (
     <div className=" ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
@@ -39,7 +35,7 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
